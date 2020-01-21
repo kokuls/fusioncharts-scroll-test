f3f0e10e64e867fbd2b21628fa01e67d3f33e35b
Kokul Subendran <ksubendran@sumologic.com>
2020-01-21 09:12:20 -0800

    Added cross line event hook

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {SafeAreaView, View,Text, StyleSheet, Platform} from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet, Platform} from 'react-native';
 import FusionCharts from 'react-native-fusioncharts';
 
 export default class App extends Component {
@@ -13,114 +13,283 @@ export default class App extends Component {
       dataFormat: 'json',
       dataSource: {
         chart: {
-          caption: 'Reach of Social Media Platforms amoung youth',
-          yaxisname: '% of youth on this platform',
-          subcaption: '2012-2016',
-          showhovereffect: '1',
-          numbersuffix: '%',
-          drawcrossline: '1',
-          plottooltext: '<b>$dataValue</b> of youth were on $seriesName',
+          caption: 'Worldwide Smartphone Sales to End Users from 2011 to 2015',
+          subcaption: 'By Top 3 Vendors',
+          linethickness: '2',
+          numberPrefix: '$',
+          showvalues: '0',
+          formatnumberscale: '1',
+          labeldisplay: 'ROTATE',
+          slantlabels: '1',
+          divLineAlpha: '40',
+          anchoralpha: '0',
+          animation: '1',
+          legendborderalpha: '20',
+          drawCrossLine: '1',
+          crossLineColor: '#0d0d0d',
+          crossLineAlpha: '100',
+          tooltipGrayOutColor: '#80bfff',
           theme: 'fusion',
         },
         categories: [
           {
             category: [
               {
-                label: '2012',
+                label: "Q1'12",
               },
               {
-                label: '2013',
+                label: "Q2'12",
               },
               {
-                label: '2014',
+                label: "Q3'12",
               },
               {
-                label: '2015',
+                label: "Q4'12",
               },
               {
-                label: '2016',
+                label: "Q1'13",
+              },
+              {
+                label: "Q2'13",
+              },
+              {
+                label: "Q3'13",
+              },
+              {
+                label: "Q4'13",
+              },
+              {
+                label: "Q1'14",
+              },
+              {
+                label: "Q2'14",
+              },
+              {
+                label: "Q3'14",
+              },
+              {
+                label: "Q4'14",
+              },
+              {
+                label: "Q1'15",
+              },
+              {
+                label: "Q2'15",
+              },
+              {
+                label: "Q3'15",
+              },
+              {
+                label: "Q4'15",
+              },
+              {
+                label: "Q1'16",
+              },
+              {
+                label: "Q2'16",
+              },
+              {
+                label: "Q3'16",
+              },
+              {
+                label: "Q4'16",
               },
             ],
           },
         ],
         dataset: [
           {
-            seriesname: 'Facebook',
+            seriesname: 'Samsung',
             data: [
               {
-                value: '62',
+                value: '716000',
               },
               {
-                value: '64',
+                value: '771700',
               },
               {
-                value: '64',
+                value: '687800',
               },
               {
-                value: '66',
+                value: '698300',
               },
               {
-                value: '78',
+                value: '826100',
+              },
+              {
+                value: '938300',
+              },
+              {
+                value: '892800',
+              },
+              {
+                value: '904300',
+              },
+              {
+                value: '979600',
+              },
+              {
+                value: '1069600',
+              },
+              {
+                value: '1006600',
+              },
+              {
+                value: '1075300',
+              },
+              {
+                value: '1170500',
+              },
+              {
+                value: '1192100',
+              },
+              {
+                value: '1100500',
+              },
+              {
+                value: '974200',
+              },
+              {
+                value: '936200',
+              },
+              {
+                value: '979900',
+              },
+              {
+                value: '887400',
+              },
+              {
+                value: '1020600',
               },
             ],
           },
           {
-            seriesname: 'Instagram',
+            seriesname: 'Nokia',
             data: [
               {
-                value: '16',
+                value: '1174600',
               },
               {
-                value: '28',
+                value: '1222800',
               },
               {
-                value: '34',
+                value: '1075600',
               },
               {
-                value: '42',
+                value: '978700',
               },
               {
-                value: '54',
+                value: '1053500',
+              },
+              {
+                value: '1117000',
+              },
+              {
+                value: '831600',
+              },
+              {
+                value: '834200',
+              },
+              {
+                value: '823000',
+              },
+              {
+                value: '850500',
+              },
+              {
+                value: '632200',
+              },
+              {
+                value: '609500',
+              },
+              {
+                value: '630600',
+              },
+              {
+                value: '635800',
+              },
+              {
+                value: '496900',
+              },
+              {
+                value: '438100',
+              },
+              {
+                value: '431300',
+              },
+              {
+                value: '330000',
+              },
+              {
+                value: '276900',
+              },
+              {
+                value: '302900',
               },
             ],
           },
           {
-            seriesname: 'LinkedIn',
+            seriesname: 'Apple',
             data: [
               {
-                value: '20',
+                value: '134800',
               },
               {
-                value: '22',
+                value: '160100',
               },
               {
-                value: '27',
+                value: '168800',
               },
               {
-                value: '22',
+                value: '196300',
               },
               {
-                value: '29',
-              },
-            ],
-          },
-          {
-            seriesname: 'Twitter',
-            data: [
-              {
-                value: '18',
+                value: '173000',
               },
               {
-                value: '19',
+                value: '354600',
               },
               {
-                value: '21',
+                value: '331200',
               },
               {
-                value: '21',
+                value: '289400',
               },
               {
-                value: '24',
+                value: '246200',
+              },
+              {
+                value: '434600',
+              },
+              {
+                value: '383300',
+              },
+              {
+                value: '319000',
+              },
+              {
+                value: '303300',
+              },
+              {
+                value: '502200',
+              },
+              {
+                value: '430600',
+              },
+              {
+                value: '353500',
+              },
+              {
+                value: '381900',
+              },
+              {
+                value: '601800',
+              },
+              {
+                value: '480900',
+              },
+              {
+                value: '460600',
               },
             ],
           },
@@ -149,6 +318,11 @@ export default class App extends Component {
             dataFormat={this.state.dataFormat}
             dataSource={this.state.dataSource}
             libraryPath={this.libraryPath} // set the libraryPath property
+            events={{
+              onChangeCrossLine: (e) => {
+                console.log('crossLine changed', e.sender.args)
+              },
+            }}
           />
         </View>
       </SafeAreaView>
